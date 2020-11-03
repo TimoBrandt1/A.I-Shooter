bf63fef51e1dc6653989bcd4b7e0ec8e07d4a40c
TimoBrandt1 <41124729+TimoBrandt1@users.noreply.github.com>
2020-11-03 19:17:52 +0100

    Created Enemy script

---
+++ A.I Shooting Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int startingHealth = 100;
+    private int currentHealth;
+
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        currentHealth = startingHealth;
+    }
+
+    private void GetShot(int damage)
+    {
+        InflictDamage(damage);
+    }
+
+    private void InflictDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Respawn();
+    }
+
+    private void Respawn()
+    {
+        currentHealth = startingHealth;
+        transform.position = startPos;
+    }
+
+}
